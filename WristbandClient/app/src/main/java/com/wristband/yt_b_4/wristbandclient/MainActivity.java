898fbbe492dbe49e8343bbe920189f9281f5e650
Michael Van Bosch <mvanbosc@iastate.edu>
2017-09-23 15:43:51 -0500

    Started android client with volley

---
+++ WristbandClient/app/src/main/java/com/wristband/yt_b_4/wristbandclient/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wristband.yt_b_4.wristbandclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
