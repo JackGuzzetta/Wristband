1d969f0ac5017be7c292fcc329b1f3c806a3a960
Michael Van Bosch <mvanbosc@iastate.edu>
2017-09-26 20:32:37 -0500

    Started work on authentication using JWT (Json web tokens)

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -1,9 +1,13 @@
 var express = require('express')
 var mysql = require('mysql');
-var jwt = require('jsonwebtoken');
+var jwt = require('jwt-simple');
+var moment = require('moment');
+
 var config = require('./config');
 var app = express()
 
+//set token to be used for authentication
+app.set('jwtTokenSecret', config.crypt);
 
 //connect to database
 var con = mysql.createConnection(config.mysql);
@@ -17,6 +21,38 @@ con.connect(function(err) {
     }
 });
 //
+
+//user auth
+//https://www.sitepoint.com/using-json-web-tokens-node-js/
+var fake_user = {};
+
+var res = {};
+fake_user.id = 12345;
+
+var expires = moment().add('days', 7).valueOf();
+
+var token = jwt.encode({
+  iss: fake_user.id,
+  exp: expires
+}, app.get('jwtTokenSecret'));
+
+console.log(token);
+// res.json({
+//   token : token,
+//   expires: expires,
+//   user: fake_user
+// });
+
+
+
+//end user auth
+
+
+
+
+
+
+
 
 //endpoints
 app.get('/get_all_users', function(req, res) {
