e7a47d08eb6382854469fa766b3a218ed37958ab
Michael Van Bosch <mvanbosc@iastate.edu>
2017-12-04 19:45:45 -0600

    server update for scanning

--- WristbandServer/routes.js
+++ WristbandServer/routes.js
@@ -220,7 +220,7 @@ module.exports = function(app) {
      * @return {json} success/fail
      */
     app.post('/scan', function(req, res) {
-        Relation.scanUser(req.headers.user_id, req.headers.party_id, req.headers.scanned_in res);
+        Relation.scanUser(req.headers.user_id, req.headers.party_id, req.headers.scanned_in, res);
     });
     /**
      * @function get: /join_user/:id
